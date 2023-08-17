e76ed1816fe7896a3b987aa0f6f7be00184fbeb5
MinseokGo <rhalstjr1999@naver.com>
2023-08-17 23:33:04 +0900

    fix: changed controller advice type

--- src/main/java/yongkinanum/backenddeploy/core/error/handler/GlobalExceptionHandler.java
+++ src/main/java/yongkinanum/backenddeploy/core/error/handler/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package yongkinanum.backenddeploy.core.error.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import yongkinanum.backenddeploy.core.error.exception.*;
 import yongkinanum.backenddeploy.core.utils.ApiUtils;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception400.class)

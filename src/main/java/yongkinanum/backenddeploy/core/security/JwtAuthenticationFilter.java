c078c2b182d7f769b92906670eed6af33402c0d5
MinseokGo <rhalstjr1999@naver.com>
2023-08-19 21:33:01 +0900

    feat: added jwt authentication filter

---
+++ src/main/java/yongkinanum/backenddeploy/core/security/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package yongkinanum.backenddeploy.core.security;
+
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import yongkinanum.backenddeploy.user.User;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jwt = request.getHeader(JwtProvider.HEADER);
+
+        if (jwt == null) {  //토큰이 없을 경우
+            chain.doFilter(request, response);  // I/O 또는 서블렛 예외 처리
+            return;
+        }
+
+        try {
+            DecodedJWT decodedJWT = JwtProvider.verify(jwt);
+            String userId = decodedJWT.getClaim("userId").asString();
+            String role = decodedJWT.getClaim("role").asString();
+            System.out.println("roles : " + role);
+
+            User user = User.builder().userId(userId).role(role).build();
+            CustomUserDetails myUserDetails = new CustomUserDetails(user);
+            Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            myUserDetails,
+                            myUserDetails.getPassword(),
+                            myUserDetails.getAuthorities()
+                    );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("디버그 : 인증 객체 만들어짐");
+        } catch (SignatureVerificationException sve) {
+            log.error("토큰 검증 실패");
+        } catch (TokenExpiredException tee) {
+            log.error("토큰 만료됨");
+        } finally {
+            chain.doFilter(request, response);
+        }
+    }
+}

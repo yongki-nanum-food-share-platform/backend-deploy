208567a5f45e9f5a7ebbaa8a2d68a3c9041ef9c2
MinseokGo <rhalstjr1999@naver.com>
2023-08-19 21:33:39 +0900

    refact: renamed enum field

--- src/main/java/yongkinanum/backenddeploy/user/Role.java
+++ src/main/java/yongkinanum/backenddeploy/user/Role.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public enum Role {
     ROLE_ADMIN("관리자"),
-    ROLE_USER("회원"),
-    ROLE_SHOPPER("가게사장"),
-    ROLE_DELIVERYMAN("배달기사");
+    ROLE_USER("일반회원"),
+    ROLE_SHOPPER("가게사장님"),
+    ROLE_DELIVERYMAN("라이더");
 
     private String description;
 

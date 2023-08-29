9292834eac58792d2412779a30ed573b05ec5599
MinseokGo <rhalstjr1999@naver.com>
2023-08-29 17:40:01 +0900

    refact: updated post select query

--- src/main/java/yongkinanum/backenddeploy/post/PostJPARepository.java
+++ src/main/java/yongkinanum/backenddeploy/post/PostJPARepository.java
@@ -3,11 +3,14 @@ package yongkinanum.backenddeploy.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import yongkinanum.backenddeploy.user.User;
 
 import java.util.List;
 
 public interface PostJPARepository extends JpaRepository<Post, Long> {
-    @Query("select p from Post p where p.user.userId = :userId")
+    @Override
+    @Query("select p from Post p where p.delete = 'N'")
+    List<Post> findAll();
+
+    @Query("select p from Post p where p.user.userId = :userId and p.delete = 'N'")
     List<Post> findByUserId(@Param("userId")String userId);
 }

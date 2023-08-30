67939ca72ff6302bbff575dbe1be894053424dbc
MinseokGo <rhalstjr1999@naver.com>
2023-08-30 16:55:15 +0900

    feat: added cart repository method
    1. find carts about user
    
    2. delete carts about user

---
+++ src/main/java/yongkinanum/backenddeploy/cart/CartJPARepository.java
@@ -0,0 +1,17 @@
+package yongkinanum.backenddeploy.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CartJPARepository extends JpaRepository<Cart, Long> {
+    @Query("select c from Cart c where c.user.idx = :idx")
+    List<Cart> findAllByUserIdx(@Param("idx") Long idx);
+
+    @Modifying
+    @Query("delete from Cart c where c.user.idx = :idx")
+    void deleteByUserId(@Param("idx")Long idx);
+}

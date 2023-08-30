2b8b5a9767760772ad5c375f8e0fdba422b17037
MinseokGo <rhalstjr1999@naver.com>
2023-08-30 16:56:21 +0900

    feat: added shop repository method

--- src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
+++ src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
@@ -1,6 +1,10 @@
 package yongkinanum.backenddeploy.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShopJPARepository extends JpaRepository<Shop, Long> {
+    @Query("select s from Shop  s where s.shopName = :shopName")
+    Shop findByShopName(@Param("shopName") String shopName);
 }

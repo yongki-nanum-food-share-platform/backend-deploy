d8214c14d7d8f1b11710c60922bdfcd04814805b
MinseokGo <rhalstjr1999@naver.com>
2023-09-02 16:27:37 +0900

    feat: added order entity

---
+++ src/main/java/yongkinanum/backenddeploy/order/Order.java
@@ -0,0 +1,33 @@
+package yongkinanum.backenddeploy.order;
+
+import lombok.*;
+import yongkinanum.backenddeploy.user.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_tb",
+        indexes = {
+                @Index(name = "order_user_idx", columnList = "user_idx")
+        })
+@Getter
+@ToString
+@NoArgsConstructor
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Setter
+    @Column(length = 1, nullable = false)
+    private Character cancel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Order(Long idx, Character cancel, User user) {
+        this.idx = idx;
+        this.cancel = cancel;
+        this.user = user;
+    }
+}

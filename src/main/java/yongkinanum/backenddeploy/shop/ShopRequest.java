a27dd66ade719a111d06cfbc38367ccba6019695
MinseokGo <rhalstjr1999@naver.com>
2023-09-07 18:20:45 +0900

    refact: updated shop request dto

--- src/main/java/yongkinanum/backenddeploy/shop/ShopRequest.java
+++ src/main/java/yongkinanum/backenddeploy/shop/ShopRequest.java
@@ -1,11 +1,15 @@
 package yongkinanum.backenddeploy.shop;
 
 import lombok.Getter;
+import lombok.Setter;
+import yongkinanum.backenddeploy.shop.brand.Brand;
+import yongkinanum.backenddeploy.user.User;
 
 import javax.validation.constraints.NotBlank;
 
 class ShopRequest {
     @Getter
+    @Setter
     public static class RegistDTO {
         @NotBlank
         private String shopName;
@@ -19,25 +23,34 @@ class ShopRequest {
         @NotBlank
         private String brandName;
 
-        public Shop toEntity() {
+        public Shop toEntity(User user, Brand brand) {
             return Shop.builder()
                     .shopName(shopName)
                     .shopAddress(shopAddress)
+                    .starPoint(0.0F)
+                    .reviewCount(0)
+                    .orderCount(0)
+                    .unregist('N')
                     .tip(Integer.parseInt(tip))
+                    .user(user)
+                    .brand(brand)
                     .build();
         }
     }
 
     @Getter
+    @Setter
     public static class UpdateDTO {
-        private String idx;
+        @NotBlank
+        private Long idx;
         private String newName;
         private String newAddress;
         private String newTip;
     }
 
     @Getter
+    @Setter
     public static class UnregistDTO {
-        private String idx;
+        private Long idx;
     }
 }

6f19f22c136b08523ae2a0c707bd8d73c040776c
MinseokGo <rhalstjr1999@naver.com>
2023-09-11 17:41:33 +0900

    chore: delete space line

--- src/main/java/yongkinanum/backenddeploy/BackendDeployApplication.java
+++ src/main/java/yongkinanum/backenddeploy/BackendDeployApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendDeployApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendDeployApplication.class, args);
     }
-
 }

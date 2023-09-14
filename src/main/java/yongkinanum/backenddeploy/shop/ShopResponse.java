4bd42d7706b33c4a57519dfdfc2a7f799d8f090a
MinseokGo <rhalstjr1999@naver.com>
2023-09-14 20:09:09 +0900

    feat: updated shop response dto

--- src/main/java/yongkinanum/backenddeploy/shop/ShopResponse.java
+++ src/main/java/yongkinanum/backenddeploy/shop/ShopResponse.java
@@ -1,12 +1,43 @@
 package yongkinanum.backenddeploy.shop;
 
 import lombok.Getter;
+import yongkinanum.backenddeploy.menu.option.Option;
+import yongkinanum.backenddeploy.order.OrderResponse;
+import yongkinanum.backenddeploy.order.item.Item;
 import yongkinanum.backenddeploy.review.Review;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ShopResponse {
+    @Getter
+    public static class FindAllDTO {
+        List<ShopDTO> shops;
+
+        public FindAllDTO(List<Shop> shops) {
+            this.shops = shops.stream()
+                    .map(ShopDTO::new)
+                    .collect(Collectors.toList());
+        }
+
+        @Getter
+        public static class ShopDTO {
+            private Long idx;
+            private String shopName;
+            private String description;
+            private Float starCount;
+
+            public ShopDTO(Shop shop) {
+                this.idx = shop.getIdx();
+                this.shopName = shop.getShopName();
+                this.description = shop.getDescription();
+                this.starCount = shop.getStarPoint();
+            }
+        }
+    }
+
     @Getter
     public static class FindDTO {
         private Long idx;
@@ -16,9 +47,10 @@ public class ShopResponse {
         private Float starPoint;
         private Integer orderCount;
         private Integer reviewCount;
+        private List<MenuDTO> menus;
         private List<ReviewDTO> reviews;
 
-        public FindDTO(List<Review> reviews, Shop shop) {
+        public FindDTO(List<Review> reviews, List<Option> options, Shop shop) {
             this.idx = shop.getIdx();
             this.shopName = shop.getShopName();
             this.shopAddress = shop.getShopAddress();
@@ -26,7 +58,43 @@ public class ShopResponse {
             this.starPoint = shop.getStarPoint();
             this.orderCount = shop.getOrderCount();
             this.reviewCount = shop.getReviewCount();
+
+            Map<String, List<Option>> OptionsByMenu = options.stream()
+                    .collect(Collectors.groupingBy(option -> option.getMenu().getMenuName()));
+
+            this.menus = OptionsByMenu.entrySet().stream()
+                    .map(entry -> new ShopResponse.FindDTO.MenuDTO(entry.getValue()))
+                    .collect(Collectors.toList());
+
             this.reviews = reviews.stream().map(ReviewDTO::new).collect(Collectors.toList());
+        }
+
+        @Getter
+        public static class MenuDTO {
+            private Long idx;
+            private String menuName;
+            private List<OptionDTO> options;
+
+            public MenuDTO(List<Option> options) {
+                this.idx = options.get(0).getMenu().getIdx();
+                this.menuName = options.get(0).getMenu().getMenuName();
+                this.options = options.stream()
+                        .map(OptionDTO::new)
+                        .collect(Collectors.toList());
+            }
+
+            @Getter
+            public static class OptionDTO {
+                private Long idx;
+                private String optionName;
+                private int price;
+
+                public OptionDTO(Option option) {
+                    this.idx = option.getIdx();
+                    this.optionName = option.getOptionName();
+                    this.price = option.getPrice();
+                }
+            }
         }
 
         @Getter

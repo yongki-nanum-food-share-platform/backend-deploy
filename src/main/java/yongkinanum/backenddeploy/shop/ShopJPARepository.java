9719bc4351e323f7a2bcec83fe70b1a92eb852d0
MinseokGo <rhalstjr1999@naver.com>
2023-09-14 20:08:41 +0900

    feat: added shop repository method

--- src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
+++ src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ShopJPARepository extends JpaRepository<Shop, Long> {
-    @Query("select s from Shop  s where s.shopName = :shopName")
+    @Query("select s from Shop s where s.shopName = :shopName")
     Shop findByShopName(@Param("shopName") String shopName);
+
+    @Query("select s from Shop s where  s.brand.brandName like %:brandName%")
+    List<Shop> findAllShopByBrandName(@Param("brandName") String brandName);
 }

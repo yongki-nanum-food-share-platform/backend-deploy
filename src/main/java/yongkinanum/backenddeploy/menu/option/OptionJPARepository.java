16085f89e2a50db60116027f3fa8467b87474cb3
MinseokGo <rhalstjr1999@naver.com>
2023-09-14 20:07:39 +0900

    feat: added option repository method

--- src/main/java/yongkinanum/backenddeploy/menu/option/OptionJPARepository.java
+++ src/main/java/yongkinanum/backenddeploy/menu/option/OptionJPARepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface OptionJPARepository extends JpaRepository<Option, Long> {
     @Query("select o from Option o where o.idx = :optionId")
     Option findByOptionId(@Param("optionId") Long optionId);
+
+    @Query("select o from Option o where o.menu.brand.idx = :idx")
+    List<Option> findAllOptionByBrandIdx(@Param("idx") Long idx);
 }

a39c16937b8c34ab9d4782b612a6bc1a971f3ac8
MinseokGo <rhalstjr1999@naver.com>
2023-09-15 17:09:55 +0900

    feat: added notice repository

---
+++ src/main/java/yongkinanum/backenddeploy/notice/NoticeJPARepository.java
@@ -0,0 +1,6 @@
+package yongkinanum.backenddeploy.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeJPARepository extends JpaRepository<Notice, Long> {
+}

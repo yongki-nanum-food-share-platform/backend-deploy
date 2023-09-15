706641a187fa52a5a5e5daad82170c7dea54e2c5
MinseokGo <rhalstjr1999@naver.com>
2023-09-15 17:11:35 +0900

    feat: added post response for search

--- src/main/java/yongkinanum/backenddeploy/post/PostResponse.java
+++ src/main/java/yongkinanum/backenddeploy/post/PostResponse.java
@@ -96,4 +96,44 @@ public class PostResponse {
             }
         }
     }
+
+    @Getter
+    public static class FindSpecificDTO {
+        List<PostDTO> posts;
+
+        public FindSpecificDTO(List<Post> posts, List<Share> shares) {
+            this.posts = posts.stream()
+                    .map(post -> new PostDTO(post, shares))
+                    .collect(Collectors.toList());
+        }
+
+        @Getter
+        public static class PostDTO {
+            private Long idx;
+            private String image;
+            private String title;
+            private String time;
+            private String place;
+            private String menuName;
+            private String people;
+
+            public PostDTO(Post post, List<Share> shares) {
+                this.idx = post.getIdx();
+                this.image = post.getShop().getBrand().getImage();
+                this.title = post.getTitle();
+                this.time = post.getTime();
+                this.place = post.getPlace();
+                this.menuName = getSelectMenuName(post, shares);
+                this.people = post.getPeople();
+            }
+
+            private String getSelectMenuName(Post post, List<Share> shares) {
+                List<Share> findShares = shares.stream()
+                        .filter(share -> share.getPost().getIdx() == post.getIdx())
+                        .collect(Collectors.toList());
+
+                return String.format("%s 외 %d건", findShares.get(0).getOption().getMenu().getMenuName(), findShares.size() - 1);
+            }
+        }
+    }
 }

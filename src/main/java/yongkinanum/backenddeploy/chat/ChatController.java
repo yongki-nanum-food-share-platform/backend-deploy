cda9d6e9606e30798b19cdce9a05b87d6ca11cd4
MinseokGo <rhalstjr1999@naver.com>
2023-09-19 18:52:09 +0900

    feat: added socket chatting api

--- src/main/java/yongkinanum/backenddeploy/chat/ChatController.java
+++ src/main/java/yongkinanum/backenddeploy/chat/ChatController.java
@@ -3,39 +3,50 @@ package yongkinanum.backenddeploy.chat;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import yongkinanum.backenddeploy.core.security.CustomUserDetails;
 import yongkinanum.backenddeploy.core.utils.ApiUtils;
 
 @RestController
-@RequestMapping("/chats")
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatService chatService;
 
-    @PostMapping("/enter")
+    @MessageMapping("/{id}")
+    @SendTo("/chats/{id}")
+    public ResponseEntity<?> receiveAndSend(@DestinationVariable Long id, ChatRequest.ReceiveDTO receiveDTO, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        ChatResponse.SendDTO sendDTO = chatService.receiveAndSendChat(id, receiveDTO, userDetails.getUser());
+
+        return ResponseEntity.ok().body(ApiUtils.success(sendDTO));
+    }
+
+
+    @PostMapping("/chats/enter")
     public ResponseEntity<?> enter(@RequestBody ChatRequest.EnterDTO enterDTO, @AuthenticationPrincipal CustomUserDetails userDetails) {
         chatService.enterChat(enterDTO, userDetails.getUser());
 
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
-    @GetMapping()
+    @GetMapping("/chats")
     public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
         ChatResponse.FindAllDTO findAllDTO = chatService.findAllChats(userDetails.getUser());
 
         return ResponseEntity.ok().body(ApiUtils.success(findAllDTO));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/chats/{id}")
     public ResponseEntity<?> find(@PathVariable Long id) {
         ChatResponse.FindDTO findDTO = chatService.findChat(id);
 
         return ResponseEntity.ok().body(ApiUtils.success(findDTO));
     }
 
-    @DeleteMapping("/{id}/delete")
+    @DeleteMapping("/chats/{id}/delete")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         chatService.deleteChat(id);
 

d681363eef6bf373c43a85d2bd1ad79facb0bc1a
MinseokGo <rhalstjr1999@naver.com>
2023-09-21 16:03:20 +0900

    fix: fixed query

--- src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
+++ src/main/java/yongkinanum/backenddeploy/shop/ShopJPARepository.java
@@ -7,12 +7,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ShopJPARepository extends JpaRepository<Shop, Long> {
-    @Query("select s from Shop s where s.shopName = :shopName")
+    @Query("select s from Shop s where s.shopName = :shopName and s.unregist = 'N'")
     Shop findByShopName(@Param("shopName") String shopName);
 
-    @Query("select s from Shop s where  s.brand.brandName like %:brandName%")
+    @Query("select s from Shop s where s.brand.brandName like %:brandName% and s.unregist = 'N'")
     List<Shop> findAllShopByBrandName(@Param("brandName") String brandName);
 
-    @Query("select s from Shop s where s.user.idx = :idx")
+    @Query("select s from Shop s where s.user.idx = :idx and s.unregist = 'N'")
     Shop findShopByUserIdx(@Param("idx") Long idx);
 }

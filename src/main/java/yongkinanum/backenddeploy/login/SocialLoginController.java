945b89b7fedc9a37c43bf7cfa6ad9a6ad19a41f7
MinseokGo <rhalstjr1999@naver.com>
2023-10-17 23:46:43 +0900

    feat: 구글 로그인 컨트롤러 작성

--- src/main/java/yongkinanum/backenddeploy/login/SocialLoginController.java
+++ src/main/java/yongkinanum/backenddeploy/login/SocialLoginController.java
@@ -14,13 +14,13 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import yongkinanum.backenddeploy.core.security.JwtProvider;
 import yongkinanum.backenddeploy.core.utils.ApiUtils;
 import yongkinanum.backenddeploy.user.Role;
 import yongkinanum.backenddeploy.user.User;
 import yongkinanum.backenddeploy.user.UserService;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
 
@@ -40,8 +40,102 @@ public class SocialLoginController {
     @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
     private String naverRedirectUri;
 
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String googleRedirectUri;
+
+
     private final UserService userService;
 
+
+    @GetMapping("/login/google")
+    public ResponseEntity<?> googleLogin() {
+        String url = String.format("https://accounts.google.com/o/oauth2/v2/auth?client_id=%s&redirect_uri=%s&response_type=code&scope=email profile", googleClientId, googleRedirectUri);
+        SocialLoginResponse.GoogleDTO googleDTO = new SocialLoginResponse.GoogleDTO();
+        googleDTO.setGoogleUri(url);
+
+        return ResponseEntity.ok().body(ApiUtils.success(googleDTO));
+    }
+
+    @PostMapping("/login/google/check")
+    public @ResponseBody ResponseEntity<?> googleCallback(@RequestBody String code) throws JsonProcessingException {
+        // 인가 코드 받아오기
+        RestTemplate rt = new RestTemplate();
+        rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", googleClientId);
+        params.add("redirect_uri", googleRedirectUri);
+        params.add("code", code);
+        params.add("client_secret", "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");
+
+        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
+        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
+        HttpEntity<MultiValueMap<String, String>> googleTokenRequest = new HttpEntity<>(params, headers);
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response = rt.exchange(
+                "https://oauth2.googleapis.com/token", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                googleTokenRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        ObjectMapper om = new ObjectMapper();
+        GoogleOAuthToken googleOAuthToken = null;
+
+        try{
+            googleOAuthToken = om.readValue(response.getBody(), GoogleOAuthToken.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("googleOAuthToken = " + googleOAuthToken);
+
+        // access_token 받아오기
+        RestTemplate rt2 = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization","Bearer "+ Objects.requireNonNull(googleOAuthToken).getAccess_token());
+        headers2.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> googleProfileRequest = new HttpEntity<>(headers2);
+
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://www.googleapis.com/oauth2/v1/userinfo", // https://{요청할 서버 주소}
+                HttpMethod.GET, // 요청할 방식
+                googleProfileRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        System.out.println("response2 = " + response2.getBody());
+
+        ObjectMapper om2 = new ObjectMapper();
+        UserGoogleProfile userGoogleProfile = null;
+
+        try{
+            userGoogleProfile = om2.readValue(response2.getBody(), UserGoogleProfile.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        User user = User.builder()
+                .userId(String.valueOf(userGoogleProfile.getEmail()))
+                .password("google")
+                .userName(userGoogleProfile.getName())
+                .role(Role.ROLE_USER.getDescription())
+                .createAt(new Date())
+                .unregist('N')
+                .build();
+
+        String jwt = userService.socialLogin(Objects.requireNonNull(user));
+
+        return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(ApiUtils.success(null));
+    }
 
     @GetMapping("/login/kakao")
     public ResponseEntity<?> kakaoLogin() {
@@ -50,6 +144,83 @@ public class SocialLoginController {
         kakaoDTO.setKakaoUri(url);
 
         return ResponseEntity.ok().body(ApiUtils.success(kakaoDTO));
+    }
+
+    @PostMapping("/login/kakao/check")
+    public @ResponseBody ResponseEntity<?> kakaoCallback(@RequestBody String code){
+        // 인가 코드 받아오기
+        RestTemplate rt = new RestTemplate();
+        rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", kakaoClientId);
+        params.add("redirect_uri", kakaoRedirectUri);
+        params.add("code", code);
+        params.add("client_secret", "zLl1gqgJnOSTUsWJBuVad86Ds1yIZd4U");
+
+        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
+        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoTokenRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        ObjectMapper om = new ObjectMapper();
+        KakaoOAuthToken kakaoOAuthToken = null;
+
+        try{
+            kakaoOAuthToken = om.readValue(response.getBody(),KakaoOAuthToken.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        // access_token 받아오기
+        RestTemplate rt2 = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization","Bearer "+ Objects.requireNonNull(kakaoOAuthToken).getAccess_token());
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
+
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://kapi.kakao.com/v2/user/me", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                kakaoProfileRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        System.out.println("response2 = " + response2);
+        ObjectMapper om2 = new ObjectMapper();
+        UserKakaoProfile userKakaoProfile = null;
+
+        try{
+            userKakaoProfile = om2.readValue(response2.getBody(),UserKakaoProfile.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        User user = User.builder()
+                .userId(String.valueOf(userKakaoProfile.getKakaoAccount().getEmail()))
+                .password("kakao")
+                .userName(userKakaoProfile.getKakaoAccount().getProfile().getNickname())
+                .role(Role.ROLE_USER.getDescription())
+                .createAt(new Date())
+                .unregist('N')
+                .build();
+
+        String jwt = userService.socialLogin(Objects.requireNonNull(user));
+
+        return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(ApiUtils.success(null));
     }
 
     @GetMapping("/login/naver")
@@ -149,83 +320,6 @@ public class SocialLoginController {
                 .build();
 
         System.out.println("user = " + user);
-
-        String jwt = userService.socialLogin(Objects.requireNonNull(user));
-
-        return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(ApiUtils.success(null));
-    }
-
-    @PostMapping("/login/kakao/check")
-    public @ResponseBody ResponseEntity<?> kakaoCallback(@RequestBody String code){
-        // 인가 코드 받아오기
-        RestTemplate rt = new RestTemplate();
-        rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", kakaoClientId);
-        params.add("redirect_uri", kakaoRedirectUri);
-        params.add("code", code);
-        params.add("client_secret", "zLl1gqgJnOSTUsWJBuVad86Ds1yIZd4U");
-
-        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
-        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-
-        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token", // https://{요청할 서버 주소}
-                HttpMethod.POST, // 요청할 방식
-                kakaoTokenRequest, // 요청할 때 보낼 데이터
-                String.class // 요청 시 반환되는 데이터 타입
-        );
-
-        ObjectMapper om = new ObjectMapper();
-        KakaoOAuthToken kakaoOAuthToken = null;
-
-        try{
-            kakaoOAuthToken = om.readValue(response.getBody(),KakaoOAuthToken.class);
-        } catch (JsonProcessingException e){
-            e.printStackTrace();
-        }
-
-        // access_token 받아오기
-        RestTemplate rt2 = new RestTemplate();
-        HttpHeaders headers2 = new HttpHeaders();
-        headers2.add("Authorization","Bearer "+ Objects.requireNonNull(kakaoOAuthToken).getAccess_token());
-        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
-
-
-        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
-        ResponseEntity<String> response2 = rt2.exchange(
-                "https://kapi.kakao.com/v2/user/me", // https://{요청할 서버 주소}
-                HttpMethod.POST, // 요청할 방식
-                kakaoProfileRequest, // 요청할 때 보낼 데이터
-                String.class // 요청 시 반환되는 데이터 타입
-        );
-
-        System.out.println("response2 = " + response2);
-        ObjectMapper om2 = new ObjectMapper();
-        UserKakaoProfile userKakaoProfile = null;
-
-        try{
-            userKakaoProfile = om2.readValue(response2.getBody(),UserKakaoProfile.class);
-        } catch (JsonProcessingException e){
-            e.printStackTrace();
-        }
-
-        User user = User.builder()
-                .userId(String.valueOf(userKakaoProfile.getKakaoAccount().getEmail()))
-                .password("kakao")
-                .userName(userKakaoProfile.getKakaoAccount().getProfile().getNickname())
-                .role(Role.ROLE_USER.getDescription())
-                .createAt(new Date())
-                .unregist('N')
-                .build();
 
         String jwt = userService.socialLogin(Objects.requireNonNull(user));
 

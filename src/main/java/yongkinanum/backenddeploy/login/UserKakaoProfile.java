3e9fd547c3f453e9d43c4016f04d4b04ab5e3eb0
MinseokGo <rhalstjr1999@naver.com>
2023-10-17 22:32:31 +0900

    refactor: 소셜 로그인 저장 정보 클래스 수정

--- src/main/java/yongkinanum/backenddeploy/login/UserKakaoProfile.java
+++ src/main/java/yongkinanum/backenddeploy/login/UserKakaoProfile.java
@@ -1,11 +1,29 @@
 package yongkinanum.backenddeploy.login;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserKakaoProfile {
-    public Long id;
-    public String connected_at;
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoAccount {
+        private String email;
+        private Profile profile;
+
+        @Getter
+        @Setter
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        static class Profile {
+            private String nickname;
+        }
+    }
 }

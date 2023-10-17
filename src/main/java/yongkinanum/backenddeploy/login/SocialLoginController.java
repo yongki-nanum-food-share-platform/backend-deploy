88c18f121848f93a953d8e16d8b80e0cd2968321
MinseokGo <rhalstjr1999@naver.com>
2023-10-17 17:40:08 +0900

    feat: 네이버 로그인 컨트롤러 등록

--- src/main/java/yongkinanum/backenddeploy/login/SocialLoginController.java
+++ src/main/java/yongkinanum/backenddeploy/login/SocialLoginController.java
@@ -1,7 +1,6 @@
 package yongkinanum.backenddeploy.login;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,20 +9,19 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 import yongkinanum.backenddeploy.core.security.JwtProvider;
 import yongkinanum.backenddeploy.core.utils.ApiUtils;
+import yongkinanum.backenddeploy.user.Role;
 import yongkinanum.backenddeploy.user.User;
-import yongkinanum.backenddeploy.user.UserJPARepository;
 import yongkinanum.backenddeploy.user.UserService;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
 @RequiredArgsConstructor
@@ -31,21 +29,119 @@ import java.util.Objects;
 public class SocialLoginController {
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String clientId;
+    private String kakaoClientId;
 
     @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String redirectUri;
+    private String kakaoRedirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    private String naverClientId;
+
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+    private String naverRedirectUri;
 
     private final UserService userService;
 
 
     @GetMapping("/login/kakao")
     public ResponseEntity<?> kakaoLogin() {
-        String url = String.format("https://kauth.kakao.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code", clientId, redirectUri);
+        String url = String.format("https://kauth.kakao.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code", kakaoClientId, kakaoRedirectUri);
         SocialLoginResponse.KakaoDTO kakaoDTO = new SocialLoginResponse.KakaoDTO();
         kakaoDTO.setKakaoUri(url);
 
         return ResponseEntity.ok().body(ApiUtils.success(kakaoDTO));
+    }
+
+    @GetMapping("/login/naver")
+    public ResponseEntity<?> naverLogin() {
+        String url = String.format("https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=%s&redirect_uri=%s&state=test", naverClientId, naverRedirectUri);
+        System.out.println("url = " + url);
+        SocialLoginResponse.NaverDTO naverDTO = new SocialLoginResponse.NaverDTO();
+        naverDTO.setNaverUri(url);
+
+        return ResponseEntity.ok().body(ApiUtils.success(naverDTO));
+    }
+
+    @PostMapping("/login/naver/check")
+    public @ResponseBody ResponseEntity<?> naverCallback(@RequestBody String code){
+        System.out.println("code = " + code);
+        // access_token 받아오기
+        RestTemplate rt = new RestTemplate();
+        rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","text/html;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", naverClientId);
+        params.add("state", "test");
+        params.add("code", code);
+        params.add("client_secret", "KO0Zw6fyUD");
+
+        // 요청하기 위해 헤더(Header)와 데이터(Body)를 합친다.
+        // kakaoTokenRequest는 데이터(Body)와 헤더(Header)를 Entity가 된다.
+        HttpEntity<MultiValueMap<String, String>> naverTokenRequest = new HttpEntity<>(params, headers);
+
+        System.out.println("11111 = " + 22222);
+
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response = rt.exchange(
+                "https://nid.naver.com/oauth2.0/token", // https://{요청할 서버 주소}
+                HttpMethod.POST, // 요청할 방식
+                naverTokenRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        System.out.println("11111 = " + 11111);
+
+
+        ObjectMapper om = new ObjectMapper();
+        NaverOAuthToken naverOAuthToken = null;
+
+        try{
+            naverOAuthToken = om.readValue(response.getBody(),NaverOAuthToken.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        RestTemplate rt2 = new RestTemplate();
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization","Bearer "+ Objects.requireNonNull(naverOAuthToken).getAccess_token());
+        headers.add("Content-type","text/json;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> naverProfileRequest = new HttpEntity<>(headers2);
+
+
+        // POST 방식으로 Http 요청한다. 그리고 response 변수의 응답 받는다.
+        ResponseEntity<String> response2 = rt2.exchange(
+                "https://openapi.naver.com/v1/nid/me", // https://{요청할 서버 주소}
+                HttpMethod.GET, // 요청할 방식
+                naverProfileRequest, // 요청할 때 보낼 데이터
+                String.class // 요청 시 반환되는 데이터 타입
+        );
+
+        ObjectMapper om2 = new ObjectMapper();
+        UserNaverProfile userNaverProfile = null;
+
+        try{
+            userNaverProfile = om2.readValue(response2.getBody(),UserNaverProfile.class);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        User user = User.builder()
+                .userId(userNaverProfile.getEmail())
+                .userName(userNaverProfile.getNickname())
+                .role(Role.ROLE_USER.getDescription())
+                .createAt(new Date())
+                .password("naver")
+                .unregist('Y')
+                .build();
+
+        String jwt = userService.socialLogin(Objects.requireNonNull(user));
+
+        return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(ApiUtils.success(null));
     }
 
     @PostMapping("/login/kakao/check")
@@ -58,8 +154,8 @@ public class SocialLoginController {
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", clientId);
-        params.add("redirect_uri", redirectUri);
+        params.add("client_id", kakaoClientId);
+        params.add("redirect_uri", kakaoRedirectUri);
         params.add("code", code);
         params.add("client_secret", "zLl1gqgJnOSTUsWJBuVad86Ds1yIZd4U");
 
@@ -110,7 +206,16 @@ public class SocialLoginController {
             e.printStackTrace();
         }
 
-        String jwt = userService.socialLogin(Objects.requireNonNull(userKakaoProfile));
+        User user = User.builder()
+                .userId(String.valueOf(userKakaoProfile.getId()))
+                .password("kakao")
+                .userName("kakao" + userKakaoProfile.getId())
+                .role(Role.ROLE_USER.getDescription())
+                .createAt(new Date())
+                .unregist('Y')
+                .build();
+
+        String jwt = userService.socialLogin(Objects.requireNonNull(user));
 
         return ResponseEntity.ok().header(JwtProvider.HEADER, jwt).body(ApiUtils.success(null));
     }

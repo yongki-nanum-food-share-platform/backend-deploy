4695e911bd2a18833046da6457eca8a03b856238
MinseokGo <rhalstjr1999@naver.com>
2023-10-17 22:32:46 +0900

    refact: 네이버 로그인 저장 정보 수정

--- src/main/java/yongkinanum/backenddeploy/login/UserNaverProfile.java
+++ src/main/java/yongkinanum/backenddeploy/login/UserNaverProfile.java
@@ -2,10 +2,20 @@ package yongkinanum.backenddeploy.login;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserNaverProfile {
-    public String nickname;
-    public String email;
+    private Response response;
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class Response {
+        private String nickname;
+        private String email;
+    }
 }
